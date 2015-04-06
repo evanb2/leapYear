835e9cf2edcd537f16a2edacbc91f050fbd9a28b
Kropff & Evan Butler <student@epicodus.com>
2015-04-06 10:46:38 -0700

    include spec and script for 400, and debug display

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var leapYear = function(year) {
-    if ((year % 4 === 0) && (year % 100 !== 0)) {
-        return true;
+    if (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)) {
+      return true;
     } else {
       return false;
     }
@@ -14,9 +14,11 @@ $(document).ready(function() {
     $(".year").text(year);
     if (!result) {
       $(".not").text("not");
+    } else {
+      $(".not").text("");
     }
 
     $("#result").show();
-    event.preventDefault();  
+    event.preventDefault();
   });
 });
